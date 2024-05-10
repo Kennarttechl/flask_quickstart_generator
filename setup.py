a2ad71d0590bf34f09e9d2233a673314938dcd33
Kennarttechl <kennarttechnologies@gmail.com>
2024-05-10 10:39:28 -0800

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(file="Readme.md", encoding="utf-8", mode="r") as file:
 
 setup(
     name="flask_boilerplate",
-    version="1.0.0",
+    version="1.0",
     description="Flask Boilerplate streamlines Flask development by automatically generating a structured folder layout, expediting project setup.",
     long_description=long_description,
     long_description_content_type="text/markdown",
