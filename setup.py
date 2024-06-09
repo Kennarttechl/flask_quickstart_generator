ed49f2fff5b13a66dc099c2b39bd2ed0c855b534
Kennarttechl <kennarttechnologies@gmail.com>
2024-06-08 20:22:07 -0800

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(file="Readme.md", encoding="utf-8", mode="r") as file:
 
 setup(
     name="flask_quickstart_generator",
-    version="1.0.1",
+    version="1.0.2",
     description="Flask Quickstart Generator streamlines Flask development by automatically generating a structured folder layout, expediting project setup.",
     long_description=long_description,
     long_description_content_type="text/markdown",
