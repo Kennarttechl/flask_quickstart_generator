be6454d1c489ed817adb9b1adc95412f38d5e796
Kennarttechl <kennarttechnologies@gmail.com>
2024-06-08 20:03:17 -0800

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(file="Readme.md", encoding="utf-8", mode="r") as file:
 setup(
     name="flask_quickstart_generator",
     version="1.0.1",
-    description="Flask Boilerplate generator streamlines Flask development by automatically generating a structured folder layout, expediting project setup.",
+    description="Flask Quickstart Generator streamlines Flask development by automatically generating a structured folder layout, expediting project setup.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Kennartech",
